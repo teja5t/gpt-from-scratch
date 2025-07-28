a4345dc465c0e1c0c8be8644e0f22b005fb4ee07
Tejas Tirthapura <tirthapuratejas@gmail.com>
2025-07-28 10:01:56 -0700

    made my own GPT2 model class

---
+++ train_gpt2.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        #key, query, and value all in one
+        self.c_attn = nn.Linear(config.n_embd, config.n_embd * 3)
+        #output (value down?)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                        .view(1, 1, config.block_size, -1))
+        
+    def forward(self, x):
+        B, T, C = x.size() 
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        
+        att = q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
+        
+        
+        
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, config.n_embd * 4)
+        self.GELU = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd)
+        
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.GELU(x)
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+        
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+        
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    
+class GPT(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd), #token embeddings
+            wpe = nn.Embedding(config.block_size, config.n_embd), #positional embeddings
+            h = nn.ModuleList(Block(config) for _ in range(config.n_layer)), #attention and MLP layers
+            ln_f = nn.LayerNorm(config.n_embd) #final layer norm
+        ))
+        
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False) #turn embeddings to tokens
+        
+    def forward(self, idx):
+        B, T = idx.size()
+        assert T < self.config.block_size
+        
+        pos = torch.arrange(0, T, dtype = torch.long, device = idx.device)
+        pos_emb = self.transformer.wpe(pos)
+        tok_emb = self.transformer.wte(idx)
+        x = tok_emb + pos_emb
+        
+        for block in self.transformer.h:
+            x = block(x)
+            
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+        return logits
